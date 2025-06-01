2ca05a1837ab999905164bfe8dc044d4109b4d0c
Yasadu De Silva <49260501+lg4492@users.noreply.github.com>
2025-06-01 17:24:03 -0500

    timeout functionality

--- broadcast_to_vcu.lua
+++ broadcast_to_vcu.lua
@@ -1,6 +1,14 @@
 -- Tick rate, in Hz
 TICK_RATE = 100
 
+TSIL_TIMEOUT_ENABLED = false
+
+-- TSIL timeout in seconds
+TSIL_TIMEOUT = 20
+
+-- TSIL timeout in # of ticks
+TSIL_TIMEOUT_TICKS = TSIL_TIMEOUT * TICK_RATE
+
 -- Channel to broadcast to VCU on (0 = CAN1, 1 = CAN2)
 CAN_BUS = 1
 
@@ -28,6 +36,8 @@ stored_BMS = 1
 
 temp = 0
 
+timeout_counter = 0
+
 setTickRate(TICK_RATE)
 -- no InitCAN() needed because racecapture automatically does that
 
@@ -39,10 +49,21 @@ function onTick()
     stored_IMD = temp
   end
 
+  -- bms and imd are sent in the same CAN message, so only
+  -- update timeout for BMS
   temp = getChannel(BMS_CHANNEL)
   if temp ~= nil then
     stored_BMS = temp
+    timeout_counter = 0
+  else
+    timeout_counter = timeout_counter + 1
   end
+
+  if TSIL_TIMEOUT_ENABLED and (timeout_counter > TSIL_TIMEOUT_TICKS) then
+    stored_BMS = 0
+    stored_IMD = 0
+  end
+
 
   -- update CAN message
   msg[IMD_INDEX] = stored_IMD  
